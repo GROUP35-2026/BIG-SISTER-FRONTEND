451b636cda2ab99031249847e69fbf41ff6f083e
victoriamarvis <victoriamarvis18@gmail.com>
2026-07-17 15:04:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import bgImage from './IMAGE.PNG';
+import bgImage from './IMAGE.png';
 import signBgImage from './SIGN.jpg';
 
 // ── Custom hook for window size ─────────────────────────────────────────────
